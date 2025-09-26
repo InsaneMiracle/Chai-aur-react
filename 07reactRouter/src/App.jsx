440d71fb98b35fc7796f7d26b33e95f5e9c624a1
yunus ansari <yunusansarikalos@gmail.com>
2025-09-27 01:57:53 +0530

    adding 07reactRouter

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div className=' w-screen h-screen flex items-center justify-center bg-red-800'>
+      <h1>Hello World</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
