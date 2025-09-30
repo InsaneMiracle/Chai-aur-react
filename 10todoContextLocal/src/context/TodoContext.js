9c2136a03afeeb03886640030f75cfc5b7634cee
yunus ansari <yunusansarikalos@gmail.com>
2025-09-30 21:54:49 +0530

    adding 10todoContextLocal

---
+++ 10todoContextLocal/src/context/TodoContext.js
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos:[
+        {id:1, todo:"Learn React", completed:false}
+    ],
+    addTodo: (todo) => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleCompleted: (id) => {}
+
+});
+
+export const useTodo = () =>{ return useContext(TodoContext)}
+
+export const TodoProvider = TodoContext.Provider;
